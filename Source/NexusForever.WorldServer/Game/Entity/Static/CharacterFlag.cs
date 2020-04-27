5369dd56b1db7f28ef8e41a3f707710f9379e3ba
Kirmmin <kirmmin@hotmail.com>
2020-04-26 20:06:45 -0700

    Entity/Player: Character Flags - WIP

---
+++ Source/NexusForever.WorldServer/Game/Entity/Static/CharacterFlag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NexusForever.WorldServer.Game.Entity.Static
+{
+    [Flags]
+    public enum CharacterFlag
+    {
+        None                  = 0x0000,
+        FriendRequestsBlocked = 0x0002,
+        IgnoreDuelRequests    = 0x0008,
+        HolomarkVisibleLeft   = 0x0040,
+        HolomarkVisibleRight  = 0x0080,
+        HolomarkVisibleBack   = 0x0100,
+        HolomarkNear          = 0x0200,
+    }
+}
---
+++ Source/NexusForever.WorldServer/Network/Message/Model/ServerCharacterFlags.cs
@@ -0,0 +1,17 @@
+﻿using NexusForever.Shared.IO.Map;
+using NexusForever.Shared.Network;
+using NexusForever.Shared.Network.Message;
+
+namespace NexusForever.WorldServer.Network.Message.Model
+{
+    [Message(GameMessageOpcode.ServerCharacterFlagsUpdated)]
+    public class ServerCharacterFlagsUpdated : IWritable
+    {
+        public uint Flags { get; set; }
+
+        public void Write(GamePacketWriter writer)
+        {
+            writer.Write(Flags);
+        }
+    }
+}
