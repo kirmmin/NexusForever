206e1f9f85a8c60163b593879022aafb8c786cbe
Kirmmin <kirmmin@hotmail.com>
2020-09-30 13:48:13 -0700

    Fix: Quest Completion with optional objectives

--- Source/NexusForever.WorldServer/Game/Entity/Simple.cs
+++ Source/NexusForever.WorldServer/Game/Entity/Simple.cs
@@ -76,6 +76,7 @@ namespace NexusForever.WorldServer.Game.Entity
 
             activator.QuestManager.ObjectiveUpdate(QuestObjectiveType.ActivateEntity, CreatureId, 1u);
             activator.QuestManager.ObjectiveUpdate(QuestObjectiveType.SucceedCSI, CreatureId, 1u);
+            activator.QuestManager.ObjectiveUpdate(QuestObjectiveType.ActivateTargetGroupChecklist, CreatureId, QuestChecklistIdx);
             
             //TODO: cast "116,Generic Quest Spell - Activating - Activate - Tier 1" by 0x07FD
             ScriptManager.Instance.GetScript<CreatureScript>(CreatureId)?.OnActivateSuccess(this, activator);
